36e8bc09891ac5c6b436d061743b1fe734730936
Gavin Dreyer <gavindreyer1@gmail.com>
2019-10-31 10:22:12 -0700

    start script

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require('express');
 const server = express(); // creating an instance of an express server
 
 server.get('/', (req, res) => {
-  res.status(200).json({ hello: 'Web 23' });
+	res.status(200).json({ hello: 'Web 23' });
 });
 
-server.listen(4000, () => {
-  console.log('\n Server running on port 4000 \n');
+// the port is coming from the environment
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+	console.log(`\n Server running on port ${port} \n`);
 });
